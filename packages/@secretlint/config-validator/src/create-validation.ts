4784e169c1d9504446366dc12edfa53e25bfed32
azu <azuciao@gmail.com>
2020-02-16 16:27:44 +0900

    feat(config-validator): support rule preset validation

--- packages/@secretlint/config-validator/src/create-validation.ts
+++ packages/@secretlint/config-validator/src/create-validation.ts
@@ -9,7 +9,23 @@ if (!fs.existsSync(tsFilePath)) {
     throw new Error(`Not found: ${tsFilePath}`);
 }
 const parsedResult = parse([tsFilePath], tsConfig);
-const typeName = "SecretLintConfigDescriptor";
-const schema = parsedResult.getType(typeName);
-const validator = printSingleTypeValidator(typeName, true, schema, "@secretlint/types", tsConfig);
-fs.writeFileSync(path.join(__dirname, "validation.ts"), validator, "utf-8");
+/**
+ * Note: isSecretLintConfigDescriptorRule is union type
+ * ajv does not work correctly for "rules[0].rules[0].id" case
+ */
+const create = (name: string) => {
+    const schema = parsedResult.getType(name);
+    const validation = printSingleTypeValidator(name, true, schema, "@secretlint/types", tsConfig, {
+        allErrors: true,
+        coerceTypes: "array",
+        format: "fast",
+        nullable: false,
+        unicode: true,
+        uniqueItems: true,
+        useDefaults: true
+    });
+    fs.writeFileSync(path.join(__dirname, `${name}-validation.ts`), validation, "utf-8");
+};
+create("SecretLintConfigDescriptor");
+create("SecretLintConfigDescriptorRule");
+create("SecretLintConfigDescriptorRulePreset");
--- packages/@secretlint/config-validator/src/index.ts
+++ packages/@secretlint/config-validator/src/index.ts
@@ -1,9 +1,50 @@
-import _validate from "./validation";
+import SecretLintConfigDescriptorValidate from "./SecretLintConfigDescriptor-validation";
+import SecretLintConfigDescriptorRuleValidate from "./SecretLintConfigDescriptorRule-validation";
+import SecretLintConfigDescriptorRulePresetValidate from "./SecretLintConfigDescriptorRulePreset-validation";
 
-export { isSecretLintConfigDescriptor } from "./validation";
-export const validate = (value: unknown): { ok: true } | { ok: false; error: Error } => {
+export const validate = (value: any): { ok: true } | { ok: false; error: Error } => {
     try {
-        _validate(value);
+        if (!Array.isArray(value.rules)) {
+            const error = new Error(`secretlintrc should have required 'rules' property.
+            
+{
+    "rules": [
+        {
+            "id": "secretlint-rule-example"
+        }
+    ]
+}
+`);
+            return {
+                ok: false,
+                error
+            };
+        }
+        SecretLintConfigDescriptorValidate(value);
+        for (const rule of value.rules) {
+            // validate as preset
+            if ("rules" in rule) {
+                try {
+                    SecretLintConfigDescriptorRulePresetValidate(rule);
+                } catch (error) {
+                    const errorMessage = error.message.replace(/SecretLintConfigDescriptorRulePreset/g, rule.id);
+                    return {
+                        ok: false,
+                        error: new Error(errorMessage)
+                    };
+                }
+            } else {
+                try {
+                    SecretLintConfigDescriptorRuleValidate(rule);
+                } catch (error) {
+                    const errorMessage = error.message.replace(/SecretLintConfigDescriptorRule/g, rule.id);
+                    return {
+                        ok: false,
+                        error: new Error(errorMessage)
+                    };
+                }
+            }
+        }
         return {
             ok: true
         };
