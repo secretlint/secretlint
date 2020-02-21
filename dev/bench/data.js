ef30b818b3e0f4111faba3ea0137462ae3e0ce70
github-action-benchmark <github@users.noreply.github.com>
2020-02-21 03:29:35 +0000

    add Secretlint benchmark (benchmarkjs) benchmark result for 0f86e5415f0c249c6f5c2dfbf44465f0c58ce56e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1582213014806,
+  "lastUpdate": 1582255775620,
   "repoUrl": "https://github.com/secretlint/secretlint",
   "entries": {
     "Secretlint benchmark": [
@@ -304,6 +304,44 @@ window.BENCHMARK_DATA = {
             "range": "±1.58%",
             "unit": "ops/sec",
             "extra": "7 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "azu@users.noreply.github.com",
+            "name": "azu",
+            "username": "azu"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0f86e5415f0c249c6f5c2dfbf44465f0c58ce56e",
+          "message": "feat(profiler): add --profiler flag (#40)\n\n* feat(profiler): add --profiler flag\r\n\r\n* chore(profiler): suppress test\r\n\r\n* chore(cli): add pattern\r\n\r\n* chore(profiler): wait for complete measure\r\n\r\n* fix(config-validator): add dependencies",
+          "timestamp": "2020-02-21T12:27:31+09:00",
+          "tree_id": "e392998f625d7afa1bb6a0a20071324ec264ed4d",
+          "url": "https://github.com/secretlint/secretlint/commit/0f86e5415f0c249c6f5c2dfbf44465f0c58ce56e"
+        },
+        "date": 1582255774995,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "run secretlint for textling.github.io",
+            "value": 1.82,
+            "range": "±0.65%",
+            "unit": "ops/sec",
+            "extra": "9 samples"
+          },
+          {
+            "name": "run secretlint for js-primer",
+            "value": 0.71,
+            "range": "±1.19%",
+            "unit": "ops/sec",
+            "extra": "6 samples"
           }
         ]
       }
