144f3dbb406cc191aeaa8553df7b29803cce5562
azu <azuciao@gmail.com>
2020-02-24 18:11:25 +0900

    chore(binary): use nexe@beta

--- packages/@secretlint/binary/src/entry.ts
+++ packages/@secretlint/binary/src/entry.ts
@@ -1,43 +1,43 @@
 import { cli, run } from "secretlint/lib/cli";
-// // workaround https://github.com/nexe/nexe/issues/711
-// import fs from "fs";
-// import path from "path";
-//
-// async function fsReaddir(dpath: string, opts: any = {}) {
-//     const dirList: string[] = await new Promise((resolve, reject) => {
-//         fs.readdir(dpath, (err, contents) => {
-//             if (err) {
-//                 reject(err);
-//             } else {
-//                 resolve(contents);
-//             }
-//         });
-//     });
-//
-//     if (opts.withFileTypes) {
-//         const dirents = dirList.map((entName) => {
-//             return new Promise((resolve, reject) => {
-//                 fs.stat(path.join(dpath, entName), (err, statObj) => {
-//                     if (err) {
-//                         reject(err);
-//                     } else {
-//                         // append name to stats object to behave like a dirent object
-//                         resolve({
-//                             name: entName,
-//                             ...statObj
-//                         });
-//                     }
-//                 });
-//             });
-//         });
-//
-//         return Promise.all(dirents);
-//     }
-//
-//     return dirList;
-// }
-//
-// fs.readdir = fsReaddir as any;
+// workaround https://github.com/nexe/nexe/issues/711
+import fs from "fs";
+import path from "path";
+
+async function fsReaddir(dpath: string, opts: any = {}) {
+    const dirList: string[] = await new Promise((resolve, reject) => {
+        fs.readdir(dpath, (err, contents) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(contents);
+            }
+        });
+    });
+
+    if (opts.withFileTypes) {
+        const dirents = dirList.map(entName => {
+            return new Promise((resolve, reject) => {
+                fs.stat(path.join(dpath, entName), (err, statObj) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        // append name to stats object to behave like a dirent object
+                        resolve({
+                            name: entName,
+                            ...statObj
+                        });
+                    }
+                });
+            });
+        });
+
+        return Promise.all(dirents);
+    }
+
+    return dirList;
+}
+
+fs.readdir = fsReaddir as any;
 // Entry Point
 run(cli.input, cli.flags).then(
     ({ exitStatus, stderr, stdout }) => {
