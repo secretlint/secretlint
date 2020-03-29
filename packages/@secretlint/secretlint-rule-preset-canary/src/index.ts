d9d93ed657b80fd673e3172ba76792261dc022f4
azu <azuciao@gmail.com>
2020-03-29 22:55:26 +0900

    feat(secretlint-rule-preset-canary): add secretlint-rule-secp256k1-privatekey

--- packages/@secretlint/secretlint-rule-preset-canary/src/index.ts
+++ packages/@secretlint/secretlint-rule-preset-canary/src/index.ts
@@ -5,20 +5,21 @@ import ruleNpm from "@secretlint/secretlint-rule-npm";
 import ruleSlack from "@secretlint/secretlint-rule-slack";
 import ruleBasicAuth from "@secretlint/secretlint-rule-basicauth";
 import rulePrivateKey from "@secretlint/secretlint-rule-privatekey";
+import ruleSecp256k1PrivateKey from "@secretlint/secretlint-rule-secp256k1-privatekey";
 
-export const rules = [ruleAWS, ruleGCP, rulePrivateKey, ruleNpm, ruleBasicAuth, ruleSlack];
+export const rules = [ruleAWS, ruleGCP, rulePrivateKey, ruleNpm, ruleBasicAuth, ruleSlack, ruleSecp256k1PrivateKey];
 export type Options = {};
 
 export const creator: SecretLintRulePresetCreator<Options> = {
     meta: {
         id: "@secretlint/secretlint-rule-preset-recommend",
         recommended: true,
-        type: "preset"
+        type: "preset",
     },
     create(context, _options) {
-        rules.forEach(rule => {
+        rules.forEach((rule) => {
             context.registerRule(rule);
         });
-    }
+    },
 };
 export default creator;
