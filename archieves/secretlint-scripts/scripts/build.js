54acac9cbda84d3e82e2262ac0adf37e5f16fe5f
azu <azuciao@gmail.com>
2021-04-25 19:19:01 +0900

    chore(deps): update all typescript

--- archieves/secretlint-scripts/scripts/build.js
+++ archieves/secretlint-scripts/scripts/build.js
@@ -11,9 +11,9 @@ const child = spawn(tscCommand, {
 });
 child.stderr.pipe(process.stderr);
 child.stdout.pipe(process.stdout);
-child.on("error", function(error) {
+child.on("error", function (error) {
     console.error(error);
 });
-child.on("close", function(code) {
+child.on("close", function (code) {
     process.exit(code);
 });
