82804145902ab83d6eb5593bbed8319ed5ed848f
azu <azuciao@gmail.com>
2021-09-13 10:02:29 +0900

    fix: add error message

--- packages/@secretlint/config-loader/src/index.ts
+++ packages/@secretlint/config-loader/src/index.ts
@@ -21,7 +21,8 @@ export class AggregateError extends Error {
     errors: Error[];
 
     constructor(errors: Error[], message: string) {
-        super(message);
+        const detailsMessage = errors.map((error) => error.message).join("\n\n");
+        super(message + "\n\n" + detailsMessage);
         this.errors = errors;
     }
 }
