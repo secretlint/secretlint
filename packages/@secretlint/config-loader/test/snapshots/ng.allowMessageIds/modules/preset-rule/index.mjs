e3b6874d5ef7fab1a3c061bbe6f6d90fa4c2f849
azu <azuciao@gmail.com>
2022-03-05 16:17:00 +0900

    test: add assertion for rule/preset impl

---
+++ packages/@secretlint/config-loader/test/snapshots/ng.allowMessageIds/modules/preset-rule/index.mjs
@@ -0,0 +1,19 @@
+import rule from "@secretlint/secretlint-rule-internal-test-pure-deps";
+
+const rules = [rule.createCreator({
+    meta: {
+        id: "example"
+    }
+})];
+export const creator = {
+    meta: {
+        id: "preset-rule",
+        recommended: true,
+        type: "preset"
+    },
+    create(context, _options) {
+        rules.forEach((rule) => {
+            context.registerRule(rule);
+        });
+    }
+};
