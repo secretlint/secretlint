a0a2f5142854c2659a897de46aec2e61865423c2
azu <azu@users.noreply.github.com>
2022-08-24 21:24:09 +0900

    fix(npm): allow `_authToken=${NPM_TOKEN}` (#303)
    * fix(npm): allow `_authToken=${NPM_TOKEN}`
    
    * docs: add link
    
    * docs: add link

--- packages/@secretlint/secretlint-rule-npm/src/index.ts
+++ packages/@secretlint/secretlint-rule-npm/src/index.ts
@@ -72,7 +72,9 @@ function reportIfFound_AuthTokenInNpmrc({
     t: SecretLintRuleMessageTranslate<typeof messages>;
 }) {
     // https://blog.npmjs.org/post/118393368555/deploying-with-npm-private-modules
-    const AuthTokenPattern = /_authToken=(.*)/g;
+    // allow _authToken=${NPM_TOKEN}
+    // https://github.com/secretlint/secretlint/issues/302
+    const AuthTokenPattern = /_authToken=([^$].*)/g;
     const results = source.content.matchAll(AuthTokenPattern);
     for (const result of results) {
         const index = result.index || 0;
