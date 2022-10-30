d70f8151ffb08ea93c343b7217ff7c1f492f06a9
azu <azu@users.noreply.github.com>
2022-10-31 08:36:06 +0900

    fix(github): remove duplicated gho- prefix  (#323)
    * fix(github): remove duplicated `gho-` prefix
    
    * chore(github): add comment

--- packages/@secretlint/secretlint-rule-github/src/index.ts
+++ packages/@secretlint/secretlint-rule-github/src/index.ts
@@ -32,7 +32,6 @@ type GITHUB_TOKEN_TYPE = "ghp" | "gho" | "ghu" | "ghs" | "ghr" | "github_pat";
 const typeMap = new Map<GITHUB_TOKEN_TYPE, string>([
     ["ghp", "GitHub personal access tokens"],
     ["gho", "OAuth access tokens"],
-    ["gho", "GitHub user-to-server tokens"],
     ["ghs", "GitHub user-to-server tokens"],
     ["ghr", "refresh tokens"],
     ["github_pat", "fine-grained personal access tokens"],
@@ -101,6 +100,7 @@ export const creator: SecretLintRuleCreator<Options> = {
         // https://github.blog/2021-04-05-behind-githubs-new-authentication-token-formats/
         const CLASSIC_GITHUB_TOKEN_PATTERN = /(?<type>ghp|gho|ghu|ghs|ghr)_[A-Za-z0-9_]{36}/g;
         // fine-grained personal access tokens. FIXME: Format of the token is unclear
+        // https://github.com/community/community/discussions/36441#discussioncomment-4014190
         const FINE_GRAINED_GITHUB_TOKEN_PATTERN = /(?<type>github_pat)_[A-Za-z0-9_]{82}/g;
         const patterns = [CLASSIC_GITHUB_TOKEN_PATTERN, FINE_GRAINED_GITHUB_TOKEN_PATTERN];
         const t = context.createTranslator(messages);
