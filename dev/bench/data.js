26c7580076b724885a9b9f44b5f36f9df9b2f686
github-action-benchmark <github@users.noreply.github.com>
2023-01-02 14:08:41 +0000

    add Secretlint benchmark (benchmarkjs) benchmark result for c8ff5f28bbecd8e6a7bb06f733a1747435479029

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1672668277467,
+  "lastUpdate": 1672668521529,
   "repoUrl": "https://github.com/secretlint/secretlint",
   "entries": {
     "Secretlint benchmark": [
@@ -14172,6 +14172,44 @@ window.BENCHMARK_DATA = {
             "name": "run secretlint for js-primer",
             "value": 0.69,
             "range": "±0.76%",
+            "unit": "ops/sec",
+            "extra": "6 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c8ff5f28bbecd8e6a7bb06f733a1747435479029",
+          "message": "chore(deps): bump json5 from 2.2.1 to 2.2.3 (#360)\n\nBumps [json5](https://github.com/json5/json5) from 2.2.1 to 2.2.3.\r\n- [Release notes](https://github.com/json5/json5/releases)\r\n- [Changelog](https://github.com/json5/json5/blob/main/CHANGELOG.md)\r\n- [Commits](https://github.com/json5/json5/compare/v2.2.1...v2.2.3)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: json5\r\n  dependency-type: indirect\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-01-02T23:00:51+09:00",
+          "tree_id": "d8c93e1579f689141f828aeb07a63be855448da0",
+          "url": "https://github.com/secretlint/secretlint/commit/c8ff5f28bbecd8e6a7bb06f733a1747435479029"
+        },
+        "date": 1672668519912,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "run secretlint for textling.github.io",
+            "value": 1.45,
+            "range": "±1.21%",
+            "unit": "ops/sec",
+            "extra": "8 samples"
+          },
+          {
+            "name": "run secretlint for js-primer",
+            "value": 0.48,
+            "range": "±1.11%",
             "unit": "ops/sec",
             "extra": "6 samples"
           }
