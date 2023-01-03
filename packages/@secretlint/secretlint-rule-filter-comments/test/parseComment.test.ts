df755395280f6314624e61b4763067d55f5cf951
azu <azu@users.noreply.github.com>
2023-01-03 20:49:23 +0900

    fix(filter-comment): fix secretlint-disable comment parsing (#372)
    * fix(filter-comment): fix secretlint-disable comment parsing
    
    * test: add tests

---
+++ packages/@secretlint/secretlint-rule-filter-comments/test/parseComment.test.ts
@@ -0,0 +1,29 @@
+import { parseComment } from "../src/parse-comment";
+import * as assert from "assert";
+
+describe("parseComment", function () {
+    it("should return [] when no option ", () => {
+        const results = parseComment("");
+        assert.deepStrictEqual(results, []);
+    });
+    it("should parse rule id: a ", () => {
+        const results = parseComment("secretlint-rule-a");
+        assert.deepStrictEqual(results, ["secretlint-rule-a"]);
+    });
+    it("should parse rule ids: a,b,c ", () => {
+        const results = parseComment(" a, b,c ");
+        assert.deepStrictEqual(results, ["a", "b", "c"]);
+    });
+    it("should ignore comment: -- ", () => {
+        const results = parseComment(" -- comment ");
+        assert.deepStrictEqual(results, []);
+    });
+    it("should parse rule ids + comment : a,b,c -- comment", () => {
+        const results = parseComment(" a,  b,   c ");
+        assert.deepStrictEqual(results, ["a", "b", "c"]);
+    });
+    it("should parse markdown comment : a,b,c -->", () => {
+        const results = parseComment("a, b,   c ");
+        assert.deepStrictEqual(results, ["a", "b", "c"]);
+    });
+});
