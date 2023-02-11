0f3ffc7db1452f99daeab485180f1f0dff4035da
azu <azu@users.noreply.github.com>
2023-02-11 10:02:05 +0900

    feat(canary): add "browser" build experimental (#405)
    * feat(canary): add "browser" build
    
    - polyfill node core packages
    - use esm
    
    * Update yarn.lock
    
    * Update packages/@secretlint/secretlint-rule-preset-canary/package.json
    
    * fix: add "types" to preset
    
    * Revert "fix: add "types" to preset"
    
    This reverts commit 585580d79428a27691b0fdfc8c5cd410035fe490.

--- packages/@secretlint/secretlint-rule-preset-canary/rollup.config.mjs
+++ packages/@secretlint/secretlint-rule-preset-canary/rollup.config.mjs
@@ -1,17 +1,31 @@
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
-import json from "@rollup/plugin-json";
-
-export default {
-    input: "src/index.ts",
-    output: [
-        {
-            dir: "lib/",
-            format: "commonjs",
-            exports: "named",
-            sourcemap: true,
-        },
-    ],
-    plugins: [resolve({ preferBuiltins: true }), commonjs(), typescript(), json()],
-};
+import nodePolyfills from 'rollup-plugin-polyfill-node';
+export default [
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                dir: "lib/",
+                format: "commonjs",
+                exports: "named",
+                sourcemap: true,
+            }
+        ],
+        plugins: [resolve({ preferBuiltins: true }), commonjs(), typescript()],
+    },
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                dir: "browser/",
+                format: "es",
+                exports: "named",
+                sourcemap: true,
+                treeshake: true
+            }
+        ],
+        plugins: [resolve({ preferBuiltins: true }), commonjs(), typescript(), nodePolyfills()],
+    }
+];
