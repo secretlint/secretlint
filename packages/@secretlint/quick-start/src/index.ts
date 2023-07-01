870579e20733272b012beddfa97a65a64f0e2c74
azu <azuciao@gmail.com>
2023-07-01 12:02:14 +0900

    refactor!(quick-start): Convert to ESM

--- packages/@secretlint/quick-start/src/index.ts
+++ packages/@secretlint/quick-start/src/index.ts
@@ -1,25 +1,11 @@
-import path from "path";
 import { cli, run } from "secretlint/cli";
 
-// Override secretlintrc with quick-start pkg if it is not defined.
-const defaultSecretlintRc = path.join(__dirname, "../config/.secretlintrc.json");
-const flags: typeof cli.flags = {
-    ...cli.flags,
-    secretlintrc: cli.flags.secretlintrc ?? defaultSecretlintRc,
+export const quickStart = async () => {
+    // Override secretlintrc with quick-start pkg if it is not defined.
+    const defaultSecretlintRc = new URL("../config/.secretlintrc.json", import.meta.url).pathname;
+    const flags: typeof cli.flags = {
+        ...cli.flags,
+        secretlintrc: cli.flags.secretlintrc ?? defaultSecretlintRc,
+    };
+    return run(cli.input, flags);
 };
-// Entry Point
-run(cli.input, flags).then(
-    ({ exitStatus, stderr, stdout }) => {
-        if (stdout) {
-            console.log(stdout);
-        }
-        if (stderr) {
-            console.error(stderr);
-        }
-        process.exit(exitStatus);
-    },
-    (error) => {
-        console.error(error);
-        process.exit(1);
-    }
-);
