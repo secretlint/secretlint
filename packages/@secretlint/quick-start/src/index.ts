b50ccf4c44395dc2a7a7a83b742eed2ed5e81457
Risu <79110363+risu729@users.noreply.github.com>
2023-07-08 13:57:51 +0900

    fix(quick-start): fix defaultSecretlintRc
    Co-authored-by: azu <azu@users.noreply.github.com>

--- packages/@secretlint/quick-start/src/index.ts
+++ packages/@secretlint/quick-start/src/index.ts
@@ -4,7 +4,8 @@ import { cli, run } from "secretlint/cli";
 
 export const quickStart = async () => {
     // Override secretlintrc with quick-start pkg if it is not defined.
-    const defaultSecretlintRc = path.resolve(fileURLToPath(import.meta.url), "../config/.secretlintrc.json");
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const defaultSecretlintRc = path.resolve(__dirname, "../config/.secretlintrc.json");
     const flags: typeof cli.flags = {
         ...cli.flags,
         secretlintrc: cli.flags.secretlintrc ?? defaultSecretlintRc,
