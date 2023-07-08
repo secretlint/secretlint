271d53de744d9b70f3b7d2574157ceb1e6c67ddf
Risu <79110363+risu729@users.noreply.github.com>
2023-07-08 13:17:16 +0900

    fix(quick-start): replace URL() by fileURLToPath

--- packages/@secretlint/quick-start/src/index.ts
+++ packages/@secretlint/quick-start/src/index.ts
@@ -1,8 +1,10 @@
+import { fileURLToPath } from "node:url";
+import path from "node:path";
 import { cli, run } from "secretlint/cli";
 
 export const quickStart = async () => {
     // Override secretlintrc with quick-start pkg if it is not defined.
-    const defaultSecretlintRc = new URL("../config/.secretlintrc.json", import.meta.url).pathname;
+    const defaultSecretlintRc = path.resolve(fileURLToPath(import.meta.url), "../config/.secretlintrc.json");
     const flags: typeof cli.flags = {
         ...cli.flags,
         secretlintrc: cli.flags.secretlintrc ?? defaultSecretlintRc,
