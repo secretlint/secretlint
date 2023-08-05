b8c131a051cc408c1b61e0bd0fa96369c9cbebc4
azu <azuciao@gmail.com>
2023-08-05 17:45:05 +0900

    fix

--- packages/@secretlint/formatter/test/index.test.ts
+++ packages/@secretlint/formatter/test/index.test.ts
@@ -27,6 +27,11 @@ describe("@secretlint/formatter", function () {
     const formatters = getFormatterList();
     for (const formatter of formatters) {
         const formatterName = formatter.name;
+        // TODO: skip json and table test if windows
+        const it =
+            process.platform === "win32" && (formatterName === "json" || formatterName === "table")
+                ? globalThis.it.skip
+                : globalThis.it;
         it(`test ${formatterName}`, async function () {
             const fixtureDir = snapshotsDir;
             const formatter = await loadFormatter({
