7678b8b75f327a8dbc2f468d562f80a04b16a602
azu <azuciao@gmail.com>
2023-11-27 21:01:39 +0900

    update script

---
+++ packages/@secretlint/secretlint-rule-preset-recommend/cp-canary-deps.cjs
@@ -0,0 +1,8 @@
+// copy devDependencies to package.json from canary
+const canaryPackageJSON = require("../secretlint-rule-preset-canary/package.json");
+const canaryDevDependencies = canaryPackageJSON.devDependencies;
+const recommendedPackageJSON = require("./package.json");
+// update devDependencies
+recommendedPackageJSON.devDependencies = Object.assign({}, recommendedPackageJSON.devDependencies, canaryDevDependencies);
+// write package.json
+fs.writeFileSync(path.join(__dirname, "package.json"), JSON.stringify(recommendedPackageJSON, null, 2));
