3593d98fb1398fa1929f7bff9af965b4df0a4104
github-action-benchmark <github@users.noreply.github.com>
2024-06-07 21:27:54 +0000

    add Secretlint benchmark (benchmarkjs) benchmark result for 378ff746157c3fb4e093b5463066961c1d651752

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1717772613000,
+  "lastUpdate": 1717795674444,
   "repoUrl": "https://github.com/secretlint/secretlint",
   "entries": {
     "Secretlint benchmark": [
@@ -29904,6 +29904,44 @@ window.BENCHMARK_DATA = {
             "name": "run secretlint for js-primer",
             "value": 0.93,
             "range": "±0.46%",
+            "unit": "ops/sec",
+            "extra": "7 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "committer": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "distinct": true,
+          "id": "378ff746157c3fb4e093b5463066961c1d651752",
+          "message": "chore(deps): update dependency @types/node to ^20.12.14",
+          "timestamp": "2024-06-07T21:26:14Z",
+          "tree_id": "97c9f3e1b0aefdb64595f56e24619e07911c464b",
+          "url": "https://github.com/secretlint/secretlint/commit/378ff746157c3fb4e093b5463066961c1d651752"
+        },
+        "date": 1717795672781,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "run secretlint for textling.github.io",
+            "value": 2.44,
+            "range": "±0.37%",
+            "unit": "ops/sec",
+            "extra": "11 samples"
+          },
+          {
+            "name": "run secretlint for js-primer",
+            "value": 0.94,
+            "range": "±1.02%",
             "unit": "ops/sec",
             "extra": "7 samples"
           }
