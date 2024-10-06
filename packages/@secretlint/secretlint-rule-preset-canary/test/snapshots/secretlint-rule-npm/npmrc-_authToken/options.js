8e62907039e07523b41f49e580bef1a618460e1e
azu <azuciao@gmail.com>
2024-10-06 18:29:49 +0900

    remove options.js

--- packages/@secretlint/secretlint-rule-preset-canary/test/snapshots/secretlint-rule-npm/npmrc-_authToken/options.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.options = void 0;
-/** @type {import("@secretlint/tester").SecretLintTestCaseOptions} */
-var options = {
-    inputFilePath: ".npmrc",
-};
-exports.options = options;
