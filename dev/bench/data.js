510ea7bf854c712482317f91f18b336fa645d065
github-action-benchmark <github@users.noreply.github.com>
2025-02-23 04:50:43 +0000

    add Secretlint benchmark (benchmarkjs) benchmark result for 10dc170e39c02aeb77f5c8aa8d0216028477f07d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1740286046886,
+  "lastUpdate": 1740286243331,
   "repoUrl": "https://github.com/secretlint/secretlint",
   "entries": {
     "Secretlint benchmark": [
@@ -34730,6 +34730,44 @@ window.BENCHMARK_DATA = {
             "name": "run secretlint for js-primer",
             "value": 0.86,
             "range": "±1.23%",
+            "unit": "ops/sec",
+            "extra": "7 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "41898282+github-actions[bot]@users.noreply.github.com",
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "10dc170e39c02aeb77f5c8aa8d0216028477f07d",
+          "message": "chore(release): v9.1.0 (#965)\n\nCo-authored-by: azu <azu@users.noreply.github.com>",
+          "timestamp": "2025-02-23T13:48:56+09:00",
+          "tree_id": "3b113704cb8e48b6ddb49b91fc9a93c25ac3192b",
+          "url": "https://github.com/secretlint/secretlint/commit/10dc170e39c02aeb77f5c8aa8d0216028477f07d"
+        },
+        "date": 1740286241876,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "run secretlint for textling.github.io",
+            "value": 2.29,
+            "range": "±0.49%",
+            "unit": "ops/sec",
+            "extra": "10 samples"
+          },
+          {
+            "name": "run secretlint for js-primer",
+            "value": 0.85,
+            "range": "±1.35%",
             "unit": "ops/sec",
             "extra": "7 samples"
           }
