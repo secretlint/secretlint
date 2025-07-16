a95b1eb4e276f1bb80edd56dc742be0317499e37
github-action-benchmark <github@users.noreply.github.com>
2025-07-16 11:09:09 +0000

    add Secretlint benchmark (benchmarkjs) benchmark result for fc785841412ef9355f435a774651e48f886e3a6c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1752620165091,
+  "lastUpdate": 1752664149266,
   "repoUrl": "https://github.com/secretlint/secretlint",
   "entries": {
     "Secretlint benchmark": [
@@ -39556,6 +39556,44 @@ window.BENCHMARK_DATA = {
             "name": "run secretlint for js-primer",
             "value": 0.57,
             "range": "±1.21%",
+            "unit": "ops/sec",
+            "extra": "6 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fc785841412ef9355f435a774651e48f886e3a6c",
+          "message": "chore(deps): update patch updates (#1116)\n\nCo-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>",
+          "timestamp": "2025-07-16T11:07:27Z",
+          "tree_id": "9e2f19d8a4f37f72db45897868c911431be5876d",
+          "url": "https://github.com/secretlint/secretlint/commit/fc785841412ef9355f435a774651e48f886e3a6c"
+        },
+        "date": 1752664147312,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "run secretlint for textling.github.io",
+            "value": 2.65,
+            "range": "±0.82%",
+            "unit": "ops/sec",
+            "extra": "11 samples"
+          },
+          {
+            "name": "run secretlint for js-primer",
+            "value": 0.57,
+            "range": "±1.59%",
             "unit": "ops/sec",
             "extra": "6 samples"
           }
