2f158bae79132e0a5c5f769c98724565425511b5
github-action-benchmark <github@users.noreply.github.com>
2025-09-14 18:02:49 +0000

    add Secretlint benchmark (benchmarkjs) benchmark result for 25d0b9452625755c9b8bea2cb4cc89c817363511

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757847236245,
+  "lastUpdate": 1757872969796,
   "repoUrl": "https://github.com/secretlint/secretlint",
   "entries": {
     "Secretlint benchmark": [
@@ -43356,6 +43356,44 @@ window.BENCHMARK_DATA = {
             "name": "run secretlint for js-primer",
             "value": 0.57,
             "range": "±1.35%",
+            "unit": "ops/sec",
+            "extra": "6 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "25d0b9452625755c9b8bea2cb4cc89c817363511",
+          "message": "chore(deps): update dependency rollup to ^4.50.1 (#1231)\n\nCo-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>",
+          "timestamp": "2025-09-14T18:01:22Z",
+          "tree_id": "c484dc8975102b23e414f548dd64bef388b1106a",
+          "url": "https://github.com/secretlint/secretlint/commit/25d0b9452625755c9b8bea2cb4cc89c817363511"
+        },
+        "date": 1757872967900,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "run secretlint for textling.github.io",
+            "value": 2.66,
+            "range": "±0.91%",
+            "unit": "ops/sec",
+            "extra": "11 samples"
+          },
+          {
+            "name": "run secretlint for js-primer",
+            "value": 0.57,
+            "range": "±0.62%",
             "unit": "ops/sec",
             "extra": "6 samples"
           }
